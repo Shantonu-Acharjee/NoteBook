a28eae6eb6aa9c2ac3a28c5a6b0e123396473820
Shantonu-Acharjee <shantonuacharjee@gmail.com>
2023-03-10 10:31:05 +0600

    SignUp Validation form Done

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,12 @@
-from flask import Blueprint, render_template, redirect
+from flask import Blueprint, render_template, redirect, request, flash
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods = ['GET', 'POST'])
 def login():
+    data = request.form#['']
+    print(data)
     return render_template('login.html')
 
 
@@ -15,6 +17,29 @@ def logout():
 
 
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods = ['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 2 characters', category= 'error')
+
+        elif len(firstName) < 2:
+            flash('First name must be greater than 1 characters', category= 'error')
+
+        elif password1 != password2:
+            flash('Password don not match.', category= 'error')
+
+
+        elif len(password1) < 4:
+            flash('Password must be greater than 3 characters', category= 'error')
+
+        else:
+            # add user to data base
+            flash('Account Created!', category= 'success')
+
     return render_template('sign_up.html')
